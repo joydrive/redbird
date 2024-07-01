614c3adc956b97b8bb6e7cf58797fb8026aee6c4
JohnDoneth <doneth7@gmail.com>
2024-07-01 11:22:50 -0400

    Allow passing in an Redis adapter module to use instead of Redix directly

---
+++ lib/redbird/adapter/behaviour.ex
@@ -0,0 +1,11 @@
+defmodule Redbird.Adapter.Behaviour do
+  @type command :: [String.Chars.t()]
+
+  @callback start_link(any()) :: {:ok, pid()} | :ignore | {:error, term()}
+
+  @callback noreply_command(command(), keyword()) :: {:ok, any()} | {:error, any()}
+
+  @callback command(command(), keyword()) :: {:ok, any()} | {:error, any()}
+
+  @callback command!(command(), keyword()) :: any()
+end
--- lib/redis.ex
+++ lib/redis.ex
@@ -1,27 +1,18 @@
 defmodule Redbird.Redis do
-  def child_spec(args) do
-    %{
-      id: Redbird.Redis,
-      start: {Redbird.Redis, :start_link, [args]}
-    }
-  end
-
-  def start_link(opts) do
-    Redix.start_link(opts)
-  end
+  alias Redbird.Config
 
   def get(key) do
-    pid()
-    |> Redix.command!(["GET", key])
+    ["GET", key]
+    |> Config.redis_adapter().command()
     |> case do
-      nil -> :undefined
-      response -> response
+      {:ok, nil} -> :undefined
+      {:ok, response} -> response
     end
   end
 
   def setex(%{key: key, value: value, seconds: seconds}) do
-    pid()
-    |> Redix.command(["SETEX", key, seconds, value])
+    ["SETEX", key, seconds, value]
+    |> Config.redis_adapter().command()
     |> case do
       {:ok, "OK"} -> :ok
       {:error, error} -> error
@@ -29,8 +20,7 @@ defmodule Redbird.Redis do
   end
 
   def del(keys) when is_list(keys) do
-    pid()
-    |> Redix.noreply_command(["DEL" | keys])
+    Config.redis_adapter().noreply_command(["DEL" | keys])
   end
 
   def del(key) when is_binary(key) do
@@ -38,11 +28,6 @@ defmodule Redbird.Redis do
   end
 
   def keys(pattern) do
-    pid()
-    |> Redix.command!(["KEYS", pattern])
-  end
-
-  def pid do
-    :redbird_phoenix_session
+    Config.redis_adapter().command!(["KEYS", pattern])
   end
 end
